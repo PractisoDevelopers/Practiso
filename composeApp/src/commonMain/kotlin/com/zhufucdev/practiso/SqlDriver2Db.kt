9588615b1794c3b1d7889c4f6548d426cc4b6314
zhufucdev <1095291434@qq.com>
2024-10-05 20:39:57 +0800

    :sparkles: datetime adapters

---
+++ composeApp/src/commonMain/kotlin/com/zhufucdev/practiso/SqlDriver2Db.kt
@@ -0,0 +1,25 @@
+package com.zhufucdev.practiso
+
+import app.cash.sqldelight.ColumnAdapter
+import app.cash.sqldelight.db.SqlDriver
+import com.zhufucdev.practiso.database.AppDatabase
+import com.zhufucdev.practiso.database.Quiz
+import kotlinx.datetime.Instant
+import kotlinx.datetime.format
+import kotlinx.datetime.format.DateTimeComponents
+
+private object DateTimeAdapter : ColumnAdapter<Instant, String> {
+    override fun decode(databaseValue: String): Instant {
+        return Instant.parse(databaseValue)
+    }
+
+    override fun encode(value: Instant): String {
+        return value.format(DateTimeComponents.Formats.ISO_DATE_TIME_OFFSET)
+    }
+
+}
+
+fun SqlDriver.toDatabase() = AppDatabase(
+    this,
+    quizAdapter = Quiz.Adapter(creationTimeISOAdapter = DateTimeAdapter, modificationTimeISOAdapter = DateTimeAdapter)
+)
