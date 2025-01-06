e012f80d241f4e11995d9f5a3246525736638fe1
zhufucdev <zhufuzhufu1@gmail.com>
2025-01-06 09:58:07 +0800

    :art: make Vibrator protocol-based

--- composeApp/src/iosMain/kotlin/com/zhufucdev/practiso/platform/HapticFeedback.ios.kt
+++ composeApp/src/iosMain/kotlin/com/zhufucdev/practiso/platform/HapticFeedback.ios.kt
@@ -2,13 +2,13 @@ package com.zhufucdev.practiso.platform
 
 import kotlinx.cinterop.ExperimentalForeignApi
 
-data class Vibrator(
-    val wobble: () -> Unit
-)
+interface Vibrator {
+    fun wobble()
+}
 
 var sharedVibrator: Vibrator? = null
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun wobbleHapticFeedback() {
-    sharedVibrator?.wobble?.invoke()
+    sharedVibrator?.wobble()
 }
