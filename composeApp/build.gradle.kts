314375cfaf8abf90a96fbac6fa2d70e16f5a1f6c
zhufucdev <zhufuzhufu1@gmail.com>
2025-01-07 13:13:00 +0800

    :bookmark: v1.0.8-alpha

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     alias(libs.plugins.skie)
 }
 
-val appVersion = "1.0.7-alpha"
+val appVersion = "1.0.8-alpha"
 
 kotlin {
     androidTarget {
@@ -121,7 +121,7 @@ android {
         applicationId = "com.zhufucdev.practiso"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 8
+        versionCode = 9
         versionName = appVersion
     }
     packaging {
