934651b549ad919a209b236f2aa7d797af49ef47
zhufucdev <zhufuzhufu1@gmail.com>
2025-01-12 23:07:56 +0800

    :sparkles: desktop icons

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -29,12 +29,15 @@ compose.desktop {
             windows {
                 upgradeUuid = "0744E069-28F6-42BD-97B5-E99E02001A53"
                 menu = true
+                iconFile = project.file("icons/icon.ico")
             }
             linux {
                 modules("jdk.security.auth")
+                iconFile = project.file("icons/icon.png")
             }
             macOS {
                 jvmArgs("-Dapple.awt.application.appearance=system")
+                iconFile = project.file("icons/icon.icns")
             }
         }
 
