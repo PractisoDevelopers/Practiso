21b6920dc58da052f715690668b4acb48b48e729
zhufucdev <zhufuzhufu1@gmail.com>
2025-04-07 09:08:27 +0800

    :bug: tap and drag conflict

--- iosApp/iosApp/Modifier/ScaleRelease.swift
+++ iosApp/iosApp/Modifier/ScaleRelease.swift
@@ -5,13 +5,18 @@ struct ScaleRelease : ViewModifier {
     let scale: Double
     let completion: () -> Void
     @State private var isPressed = false
+    @State private var changes = 0
     
     func body(content: Content) -> some View {
         content
             .simultaneousGesture(
                 DragGesture(minimumDistance: 0)
+                    .onChanged{ _ in
+                        changes += 1
+                    }
                     .onEnded { gesture in
-                        if gesture.translation.width > 3 || gesture.translation.height > 3 {
+                        if changes > 1 || gesture.translation.width > 3 || gesture.translation.height > 3 {
+                            changes = 0
                             return
                         }
                         withAnimation(.linear(duration: 0.1)) {
