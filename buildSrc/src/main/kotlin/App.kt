6dec2c03aeadb6050585dfe1e90899e118936ecd
zhufucdev <zhufuzhufu1@gmail.com>
2025-05-11 17:14:16 +0800

    :bug: usearch on jvm platform

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -1,4 +1,4 @@
-const val appVersion = "1.0.9-alpha"
+const val appVersion = "1.0.10-alpha"
 
 data class AndroidSdk(val min: Int, val target: Int)
 data class AndroidApp(val sdk: AndroidSdk, val versionCode: Int)
