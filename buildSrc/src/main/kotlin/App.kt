f5b44369bdd2a707f9dd6acd65eeef7baa3a8038
zhufucdev <zhufuzhufu1@gmail.com>
2025-06-03 09:21:19 +0800

    :bookmark: v1.8.1-alpha

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -1,6 +1,6 @@
-const val appVersion = "1.8.0-alpha"
+const val appVersion = "1.8.1-alpha"
 
 data class AndroidSdk(val min: Int, val target: Int)
 data class AndroidApp(val sdk: AndroidSdk, val versionCode: Int)
 
-val androidApp = AndroidApp(sdk = AndroidSdk(min = 27, target = 35), versionCode = 29)
+val androidApp = AndroidApp(sdk = AndroidSdk(min = 27, target = 35), versionCode = 30)
