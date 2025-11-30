82f9e61ecdf4794a8d131ff54f1512ceea164eac
zhufucdev <zhufuzhufu1@gmail.com>
2025-12-01 00:34:27 +0800

    :bug: race condition

--- androidApp/src/main/kotlin/com/zhufucdev/practiso/GmsTaskConvert.kt
+++ androidApp/src/main/kotlin/com/zhufucdev/practiso/GmsTaskConvert.kt
@@ -1,13 +1,17 @@
 package com.zhufucdev.practiso
 
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
+@OptIn(InternalCoroutinesApi::class)
 suspend fun <T> Task<T>.await(): T = suspendCancellableCoroutine { continuation ->
     addOnCompleteListener {
-        continuation.resume(it.result)
+        val token = continuation.tryResume(it.result)
+        if (token != null) {
+            continuation.completeResume(token)
+        }
     }
     addOnCanceledListener {
         continuation.cancel()
